ca51c3037e4bba64ac464f69496e38e1d03c4bb8
bbarovsky <barovskiyboris@gmail.com>
2022-04-29 21:02:12 +0300

    start write brand controller

--- server/controllers/brandController.js
+++ server/controllers/brandController.js
@@ -1,9 +1,14 @@
+const {Brand}  = require('../models/models')
+
 class BrandController {
     async getAll(req, res) {
-
+        const brands = await Brand.findAll()
+        return res.json(brands)
     }
     async create(req, res) {
-
+        const {name} = req.body
+        const brand = await Brand.create({name})
+        return res.json(brand)
     }
 }
 
