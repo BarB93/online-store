60fabed35df0464bcbfe3f95f312eeda165e7d44
bbarovsky <barovskiyboris@gmail.com>
2022-05-15 14:39:56 +0300

    used context in AppRouter component

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {Routes, Route, Navigate} from 'react-router-dom'
 import { authRoutes, publicRoutes } from '../routes'
 import { SHOP_ROUTE } from '../utils/consts'
+import { Context } from '../index';
 
 const AppRouter = () => {
-    const isAuth = true
+    const {user} = useContext(Context)
+    
     return (
         <Routes>   
-            {isAuth && authRoutes.map(({path, Component}) => <Route key={path} path={path} element={<Component />}/>)}
+            {user.isAuth && authRoutes.map(({path, Component}) => <Route key={path} path={path} element={<Component />}/>)}
             {publicRoutes.map(({path, Component}) => <Route key={path} path={path} element={<Component />}/>)}
             <Route
                 path="*"
