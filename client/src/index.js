7718b679807971f378e74cfe472e8b47101fc4c9
bbarovsky <barovskiyboris@gmail.com>
2022-05-15 14:38:29 +0300

    added react context

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { createContext } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import UserStore from './store/UserStore'
+import DeviceStore from './store/DeviceStore';
 
+export const Context = createContext(null)
+
+const store = {
+  user: new UserStore(),
+  device: new DeviceStore(),
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
+  <Context.Provider value={store} >
     <App />
-  </React.StrictMode>
+  </Context.Provider>
 )
 
