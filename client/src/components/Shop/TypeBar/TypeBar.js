46b8eb7d813e5fa1692ad84daf1dd99b01bf8ae8
bbarovsky <barovskiyboris@gmail.com>
2022-05-18 21:52:12 +0300

    moved TypeBar component to separate folder

--- client/src/components/Shop/TypeBar/TypeBar.js
+++ client/src/components/Shop/TypeBar/TypeBar.js
@@ -1,7 +1,7 @@
 import React, {useContext} from 'react'
 import { observer } from 'mobx-react-lite'
 
-import { Context } from '../../index'
+import { Context } from '../../../index'
 
 import styles from './TypeBar.module.scss'
 
