d0ad3e70e078ffb14834bfd8f63c2c7213d14247
bbarovsky <barovskiyboris@gmail.com>
2022-05-18 22:50:40 +0300

    moved styles for Button comoponent into separate file

--- client/src/components/UI/Button/Button.js
+++ client/src/components/UI/Button/Button.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
+import styles from './Button.module.scss'
+
 const Button = ({secondary, clickHandler, children, className, ...props}) => {
-    let classes = 'btn '
-    classes += secondary ? 'secondary ' : ''
+    let classes = styles.btn + ' '
+    classes += secondary ? styles.secondary + ' ' : ''
     classes += className ? className : ''
 
     return (
