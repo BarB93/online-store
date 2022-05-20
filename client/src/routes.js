1bd69fb94383b16ac2a427511e75162e2e680cb8
bbarovsky <barovskiyboris@gmail.com>
2022-05-21 01:59:47 +0300

    fixed routes.js

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,9 +1,9 @@
-import Admin from "./pages/Admin"
-import Auth from "./pages/Auth/Auth"
-import Basket from "./pages/Basket"
-import DevicePage from "./pages/DevicePage"
-import Shop from "./pages/Shop/Shop"
-import { ADMIN_ROUTE, BASKET_ROUTE, DEVICE_ROUTE, LOGIN_ROUTE, REGISTRATION_ROUTE, SHOP_ROUTE } from "./utils/consts"
+import Admin from './pages/Admin'
+import Auth from './pages/Auth/Auth'
+import Basket from './pages/Basket'
+import DevicePage from './pages/DevicePage/DevicePage'
+import Shop from './pages/Shop/Shop'
+import { ADMIN_ROUTE, BASKET_ROUTE, DEVICE_ROUTE, LOGIN_ROUTE, REGISTRATION_ROUTE, SHOP_ROUTE } from './utils/consts'
 
 export const authRoutes = [
     { 
