7f1fc9c4ad0e5d30bbc2436d494692ef6d5f9798
bbarovsky <barovskiyboris@gmail.com>
2022-05-25 05:45:55 +0300

    making DevicePaage component and styles for it

--- client/src/pages/DevicePage/DevicePage.js
+++ client/src/pages/DevicePage/DevicePage.js
@@ -3,6 +3,8 @@ import React from 'react'
 import Container from '../../components/UI/Container/Container'
 
 import styles from './DevicePage.module.scss'
+import pricePrettify from '../../utils/pricePrettify';
+import Button from '../../components/UI/Button/Button'
 
 const DevicePage = () => {
     const device =  {id: 1, name: 'Note 12', prece: 15, rating: 5, price: 15000, img: 'https://img.giznext.com/assets/model/4/13039/xiaomi-mobiles-xiaomi-redmi-note-12-pro-max-1626950748.jpg?width=160'}
@@ -18,14 +20,20 @@ const DevicePage = () => {
     return (
         <Container>
             <article className={styles.container}>
-                <h1 className={styles.title}>{device.name}</h1>
+                <h1 className={styles.name}>{device.name}</h1>
                 <div className={styles.content}>
                     <div className={styles.image}>
                         <img src={device.img} alt={device.name} />
                     </div>
-                    <div className={styles.price}>Цена</div>
+                    <div className={styles.info}>
+                        <div className={styles.info__header}>
+                            <div className={styles.info__name}>Цена:</div>
+                            <div className={styles.info__price}>{pricePrettify(device.price)} ₽</div>
+                        </div>
+                        <Button className={styles.info__addToCard} secondary>Добавить в корзину</Button>
+                    </div>
                     <div className={styles.features}>
-                       <h3 className={styles.features__title}>Характеристики</h3>
+                       <h3 className={styles.title + ' ' + styles.features__title}>Характеристики</h3>
                        <ul className={styles.features__body}>
                             {features.map(({id, name, value}) => 
                                 <li key={id} 
