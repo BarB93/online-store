35aec3b61e181ee42dadd8ab84aaadeca4c2a7ac
bbarovsky <barovskiyboris@gmail.com>
2022-05-27 17:34:24 +0300

    edited Modal component, added button close, title and some styles

--- client/src/components/UI/Modal/Modal.js
+++ client/src/components/UI/Modal/Modal.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import styles from './Modal.module.scss'
 
-const Modal = ({active, setActive, children}) => {
+const Modal = ({active, setActive, children, title}) => {
     const activeClass = active ? ` ${styles.active}` : ''
 
     const closeHandler = () => { setActive(false) }
@@ -13,9 +13,11 @@ const Modal = ({active, setActive, children}) => {
 
     return (
         <div className={styles.modal + activeClass} onClick={closeHandler}>
-           <div className={styles.modal__content} onClick={contentClickHandler}>
-                {children}
-           </div>
+                <div className={styles.modal__content} onClick={contentClickHandler}>
+                        {title && <h3 className={styles.modal__title}>{title}</h3>}
+                        {children}
+                        <div className={styles.modal__btnClose} onClick={closeHandler}></div>
+                </div>
         </div>
     )
 }
