42b1aa179e79fe21baa19354e8603c1e85ce704f
bbarovsky <barovskiyboris@gmail.com>
2022-05-29 04:21:38 +0300

    making Admin page component, modals for added new brands, types and devices

--- client/src/pages/Admin/Admin.js
+++ client/src/pages/Admin/Admin.js
@@ -3,6 +3,9 @@ import React, { useState } from 'react'
 import Button from '../../components/UI/Button/Button'
 import Container from '../../components/UI/Container/Container'
 import Modal from '../../components/UI/Modal/Modal'
+import CreateType from '../../components/forms/CreateType'
+import CreateBrand from '../../components/forms/CreateBrand'
+import CreateDevice from '../../components/forms/CreateDevice'
 
 import styles from './Admin.module.scss'
 
@@ -11,27 +14,33 @@ const Admin = () => {
     const [brandModal, setBrandModal] = useState(false)
     const [deviceModal, setDeviceModal] = useState(false)
 
-    const toggleTypeModal = () => {
-        setTypeModal(prev => !prev)
+    const openTypeModal = () => {
+        setTypeModal(true)
     }
-    const toggleBrandModal = () => {
-        setBrandModal(prev => !prev)
+    const openBrandModal = () => {
+        setBrandModal(true)
     }
-    const toggleDeviceModal = () => {
-        setDeviceModal(prev => !prev)
+    const openDeviceModal = () => {
+        setDeviceModal(true)
     }
+
+    const titleType = 'Добавить тип'
+    const titleBrand = 'Добавить бренд'
+    const titleDevice = 'Добавить устройство'
 
     return (
         <Container>
             <div className={styles.container}>
                 <div className={styles.buttons}>
-                    <Button className={styles.btn} onClick={toggleTypeModal}>Добавить тип</Button>
-                    <Button className={styles.btn} >Добавить бренд</Button>
-                    <Button className={styles.btn} >Добавить товар</Button>
+                    <Button className={styles.btn} onClick={openTypeModal}>{titleType}</Button>
+                    <Button className={styles.btn} onClick={openBrandModal}>{titleBrand}</Button>
+                    <Button className={styles.btn} onClick={openDeviceModal}>{titleDevice}</Button>
                 </div>
             </div>
 
-            <Modal active={typeModal} setActive={setTypeModal}></Modal>
+            <Modal active={typeModal} setActive={setTypeModal} title={titleType}><CreateType /></Modal>
+            <Modal active={brandModal} setActive={setBrandModal} title={titleBrand}><CreateBrand /></Modal>
+            <Modal active={deviceModal} setActive={setDeviceModal} title={titleDevice}><CreateDevice /></Modal>
         </Container>
     )
 }
