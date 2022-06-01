b847a5ff76a870ca7a2759068ce65f8465daf618
bbarovsky <barovskiyboris@gmail.com>
2022-06-01 20:09:25 +0300

    added new to CustomSelect component

--- client/src/components/UI/CustomSelect.js
+++ client/src/components/UI/CustomSelect.js
@@ -4,13 +4,27 @@ import Select from 'react-select'
 const defaultValue = (options, value) => options ? options.find((option) => option.value === value) : ''
 
 
-const CustomSelect = ({onChange, options, value, className}) => {
+const CustomSelect = ({onChange, options, value, className, ...rest}) => {
+
+    const customStyles = {
+        control: (base, state) => ({
+            ...base,
+            border: state.isFocused ? '1px solid #000' : '1px solid hsl(0deg, 0%, 80%)',
+            boxShadow: state.isFocused ?  '0 0 0 1px #000' : 'none',
+            '&:hover': {
+                border: state.isFocused ? '1px solid #000' : '1px solid hsl(0deg, 0%, 70%)',
+            }
+        })
+    }
+
     return (
         <div className={className}>
             <Select
+                styles={customStyles}
                 options={options}
                 onChange={value => onChange(value)}
                 value={defaultValue(options, value)}
+                {...rest}
             />
         </div>
     )
