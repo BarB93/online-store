c7ef94ff3d05eb13ece6fb89739461c467fe5113
bbarovsky <barovskiyboris@gmail.com>
2022-06-01 20:06:23 +0300

    fixed Modal component, change click event to moseDown and mouseUp

--- client/src/components/UI/Modal/Modal.js
+++ client/src/components/UI/Modal/Modal.js
@@ -4,16 +4,22 @@ import styles from './Modal.module.scss'
 
 const Modal = ({active, setActive, children, title}) => {
     const activeClass = active ? ` ${styles.active}` : ''
+    let overlayChecker = false
 
     const closeHandler = () => { setActive(false) }
-    const contentClickHandler = (event) => {
-        event.stopPropagation()
+    const mouseDownHandler = () => { 
+        overlayChecker = true 
     }
-
-
+    const mouseUpHandler = () => {
+        if(overlayChecker) {
+            setActive(false)
+            overlayChecker = false
+        }
+    }
+    
     return (
-        <div className={styles.modal + activeClass} onClick={closeHandler}>
-                <div className={styles.modal__content} onClick={contentClickHandler}>
+        <div className={styles.modal + activeClass} onMouseDown={mouseDownHandler} onMouseUp={mouseUpHandler}>
+                <div className={styles.modal__content} onClick={e => e.stopPropagation()} onMouseDown={e => e.stopPropagation()}>
                         {title && <h3 className={styles.modal__title}>{title}</h3>}
                         {children}
                         <div className={styles.modal__btnClose} onClick={closeHandler}></div>
