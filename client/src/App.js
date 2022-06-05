f540e25f5cab17727bb9d60e7730a88912f435d8
bbarovsky <barovskiyboris@gmail.com>
2022-06-05 20:51:58 +0300

    added in App.js, checking auth when a component did mount(hook useEffect)

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,38 @@
-import {BrowserRouter} from 'react-router-dom'
+import React, {useContext, useEffect} from 'react'
+import { BrowserRouter  } from 'react-router-dom'
+import { observer } from 'mobx-react-lite'
+
+import { check } from './http/userAPI'
+import { Context } from './index'
 import AppRouter from './components/AppRouter'
 import NavBar from './components/NavBar/NavBar'
 
 import './styles/normalize.scss'
 import './styles/styles.scss'
 
-function App() {
+const App = observer(() => {
+  const {user} = useContext(Context)
+
+  useEffect(() => {
+    check().then(({data}) => {
+      user.setIsLoading(false)
+      user.setIsAuth(true)
+    })
+    .catch(e => {
+      user.setIsAuth(false)
+    })
+    .finally(() => {
+      user.setIsLoading(false)
+    })
+    
+  },[])
+
   return (
     <BrowserRouter>
       <NavBar />
       <AppRouter />
     </BrowserRouter>
   )
-}
+})
 
 export default App
