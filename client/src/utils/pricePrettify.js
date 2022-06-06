aa55d22b59db458d97a689b2d1bedce4d3600fba
bbarovsky <barovskiyboris@gmail.com>
2022-06-06 12:27:49 +0300

    added check conditions for num arg in pricePrettify function

--- client/src/utils/pricePrettify.js
+++ client/src/utils/pricePrettify.js
@@ -1,4 +1,9 @@
 export default function pricePrettify (num) {
+    if(num === null || num === undefined) {
+        console.log('Неккоректная цена')
+        return 0
+    }
+
     var n = num.toString();
     var separator = " ";
     return n.replace(/(\d{1,3}(?=(?:\d\d\d)+(?!\d)))/g, "$1" + separator);
