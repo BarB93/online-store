ab240796097609bdabfb0bc173f68994269026c3
bbarovsky <barovskiyboris@gmail.com>
2022-06-08 13:07:57 +0300

    added in CreateTypeForm component, catch errors and display it in form

--- client/src/components/forms/CreateTypeForm.js
+++ client/src/components/forms/CreateTypeForm.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Field, Form, Formik } from 'formik'
 import * as Yup from 'yup'
 
@@ -6,7 +6,8 @@ import Button from '../UI/Button/Button'
 import ErrorMessage from './ErrorMessage'
 import { createType } from '../../http/deviceAPI'
 
-const CreateTypeForm = () => {
+const CreateTypeForm = ({submittedHandler}) => {
+    const [error, setError] = useState(null)
     const validationSchema = Yup.object({
         name: Yup.string().required('Укажите название типа')
     })
@@ -15,16 +16,25 @@ const CreateTypeForm = () => {
         <Formik
             initialValues={{name: ''}}
             validationSchema={validationSchema}
-            onSubmit={values => {
-                createType(values).then(data => console.log(data))
+            onSubmit={(values, {resetForm}) => {
+                createType(values).then(data => {
+                    if(error) setError(null)
+                    resetForm()
+                    if(typeof submittedHandler === 'function') {
+                        submittedHandler()
+                    }
+                }).catch((e) => {
+                    setError(e.response.data.message)
+                }) 
             }}
         >
             {({isValid, errors}) => (
-                <Form>
+                <Form onChange={() => setError(null)}>
                     <label className='form__label' htmlFor='name'>Название</label>
                     <Field className='form__input' name='name' type='text' />
                     <div className='form__footer'>
                         {errors.name && <ErrorMessage message={errors.name}/>}
+                        {error && <ErrorMessage message={error}/>}
                         <Button className='form__btn' disabled={!isValid} type='submit' secondary>Добавить</Button>
                     </div>
                 </Form>
