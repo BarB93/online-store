97ad6be5f1b77fb6902f889e4edde91e236b86f6
bbarovsky <barovskiyboris@gmail.com>
2022-06-08 09:49:51 +0300

    created SpinnerRoller component

---
+++ client/src/components/UI/spinners/SpinnerRoller/SpinnerRoller.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import styles from './SpinnerRoller.module.scss'
+
+const SpinnerRoller = () => {
+    return (
+        <div class={styles.spinner}><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+    )
+}
+
+export default SpinnerRoller
