ee1935df1f7a9db0411100cbaf6e111b8287dcbd
bbarovsky <barovskiyboris@gmail.com>
2022-06-08 13:06:19 +0300

    added in typeController, checking if is exist type name in DB

--- server/controllers/typeController.js
+++ server/controllers/typeController.js
@@ -1,10 +1,17 @@
 const {Type}   = require('../models/models')
 const ApiError = require('../error/ApiError')
 class TypeController {
-    async create(req, res) {
-        const {name} = req.body
-        const type = await Type.create({name}) 
-        return res.json(type)   
+    async create(req, res, next) {
+        try {
+            const {name} = req.body
+            let type = await Type.findOne({where: {name}})
+            if(type !== null) return next(ApiError.badRequest('Тип с таким названием уже существует'))
+            type = await Type.create({name}) 
+            return res.json(type)   
+
+        } catch(e) {
+            next(ApiError.badRequest(e))
+        }
     }
 
     async getAll(req, res) {
