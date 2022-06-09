2e6591b1209c9299fcfa4a55cafc72bb8601d890
bbarovsky <barovskiyboris@gmail.com>
2022-06-09 06:43:21 +0300

    added isFetchingAuth var in UserStore mobx

--- client/src/store/UserStore.js
+++ client/src/store/UserStore.js
@@ -5,30 +5,32 @@ export default class UserStore {
         this._isAuth = false
         this._user = {}
         this._isLoading = true
+        this._isFetchingAuth = false
         makeAutoObservable(this)
     }
 
     setIsAuth(bool) {
         this._isAuth = bool
     }
-
     setUser(user) {
         this._user = user
     }
-
     setIsLoading(bool) {
         this._isLoading = bool
     }
-
+    setIsFetchingAuth(bool) {
+        this._isFetchingAuth = bool
+    }
     get isAuth() {
         return this._isAuth
     }
-
     get user() {
         return this._user
     }
-
     get isLoading() {
         return this._isLoading
+    }
+    get  isFetchingAuth() {
+        return this._isFetchingAuth
     }
 }
