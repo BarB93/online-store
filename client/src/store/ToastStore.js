f0d941bf2c696b3a54a0449ba584ed82ab83fa95
bbarovsky <barovskiyboris@gmail.com>
2022-06-09 14:50:50 +0300

    added count var in ToastStore mobx

--- client/src/store/ToastStore.js
+++ client/src/store/ToastStore.js
@@ -3,13 +3,21 @@ import { makeAutoObservable } from "mobx";
 export default class ToastStore {
     constructor() {
         this._list = []
+        this._count = 0
         makeAutoObservable(this)
     }
     
+    addToast(toast) {
+        this._count += 1
+        this._list = [...this._list, toast]
+    }
     setList(list) {
         this._list = list
     }
     get list() {
         return this._list
+    }
+    get count() {
+        return this._count
     }
 }
