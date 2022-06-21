0e3171d8778dc32652a990748c0aeea28daf0279
bbarovsky <barovskiyboris@gmail.com>
2022-06-22 00:38:32 +0300

    fixed styles for DeviceItem component

--- client/src/components/Shop/DeviceList/DeviceList.js
+++ client/src/components/Shop/DeviceList/DeviceList.js
@@ -9,7 +9,7 @@ import styles from './DeviceList.module.scss'
 
 const DeviceList = observer(() => {
     const {device, user} = useContext(Context)
-    
+
     return (
         <div className={styles.list}>
             { (user.isLoading || device.isLoadingDevices) ? new Array(device.limit).fill(0).map((i, index) => <DeviceItemSkeleton key={index} />)
