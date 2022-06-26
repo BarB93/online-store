e3d36574f4a52d13ed9ecc017a6d848d9fb67704
bbarovsky <barovskiyboris@gmail.com>
2022-06-26 22:31:36 +0300

    added styles for CustomSelect component

--- client/src/components/UI/CustomSelect.js
+++ client/src/components/UI/CustomSelect.js
@@ -5,15 +5,15 @@ const defaultValue = (options, value) => options ? options.find((option) => opti
 
 
 const CustomSelect = ({onChange, options, value, className, ...rest}) => {
-
     const customStyles = {
         control: (base, state) => ({
             ...base,
             border: state.isFocused ? '1px solid #000' : '1px solid hsl(0deg, 0%, 80%)',
             boxShadow: state.isFocused ?  '0 0 0 1px #000' : 'none',
+            cursor: 'pointer',
             '&:hover': {
                 border: state.isFocused ? '1px solid #000' : '1px solid hsl(0deg, 0%, 70%)',
-            }
+            },
         })
     }
 
