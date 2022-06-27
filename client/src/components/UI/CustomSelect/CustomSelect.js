ec6ff1e301e589d82ec928ea338c12d8a8338148
bbarovsky <barovskiyboris@gmail.com>
2022-06-27 16:52:22 +0300

    canged CustomSelect component: added custom styles

--- client/src/components/UI/CustomSelect/CustomSelect.js
+++ client/src/components/UI/CustomSelect/CustomSelect.js
@@ -1,26 +1,17 @@
 import React from 'react'
 import Select from 'react-select'
 
+import './CustomSelect.scss'
+
 const defaultValue = (options, value) => options ? options.find((option) => option.value === value) : ''
 
 
 const CustomSelect = ({onChange, options, value, className, ...rest}) => {
-    const customStyles = {
-        control: (base, state) => ({
-            ...base,
-            border: state.isFocused ? '1px solid #000' : '1px solid hsl(0deg, 0%, 80%)',
-            boxShadow: state.isFocused ?  '0 0 0 1px #000' : 'none',
-            cursor: 'pointer',
-            '&:hover': {
-                border: state.isFocused ? '1px solid #000' : '1px solid hsl(0deg, 0%, 70%)',
-            },
-        })
-    }
-
+   
     return (
         <div className={className}>
             <Select
-                styles={customStyles}
+                classNamePrefix={'customSelect'}
                 options={options}
                 onChange={value => onChange(value)}
                 value={defaultValue(options, value)}
