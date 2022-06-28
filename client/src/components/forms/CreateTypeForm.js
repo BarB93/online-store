c7166032b620e7d0b1a01cfed499159016882f57
bbarovsky <barovskiyboris@gmail.com>
2022-06-28 15:42:10 +0300

    added in CreateTypeForm component: use i18next translation

--- client/src/components/forms/CreateTypeForm.js
+++ client/src/components/forms/CreateTypeForm.js
@@ -9,12 +9,14 @@ import { createType } from '../../http/deviceAPI'
 import SpinnerFacebook from '../UI/spinners/SpinnerFacebook/SpinnerFacebook'
 import Button from '../UI/Button/Button'
 import ErrorMessage from './ErrorMessage'
+import { useTranslation } from 'react-i18next';
 
 const CreateTypeForm = observer(({submittedHandler}) => {
+    const i18n = useTranslation()
     const {toast, type} = useContext(Context)
     const [error, setError] = useState(null)
     const validationSchema = Yup.object({
-        name: Yup.string().required('Укажите название типа')
+        name: Yup.string().required(i18n.t('Enter type name'))
     })
     const onSubmit = async (values, {resetForm, ...rest}) => {
         try {
@@ -22,7 +24,7 @@ const CreateTypeForm = observer(({submittedHandler}) => {
             type.setIsFetchingType(true)
             await createType(values)
             resetForm()
-            toast.addToast(createToast(`Тип ${values.name} успешно создан!`))
+            toast.addToast(createToast(i18n.t('Type created successfully'), {name: values.name}))
             if(typeof submittedHandler === 'function') {
                 submittedHandler()
             }
@@ -41,23 +43,23 @@ const CreateTypeForm = observer(({submittedHandler}) => {
 
     return (
         <form onSubmit={formik.handleSubmit} onChange={() => setError(null)}>   
-            <label className='form__label' htmlFor='name'>Название</label>
+            <label className='form__label' htmlFor='name'>{i18n.t('Name of type')}</label>
             <input 
                 className='form__input'
                 id="name"
                 type='text'
-                placeholder='Введите название типа...'
+                placeholder={i18n.t('Enter type name...')}
                 {...formik.getFieldProps('name')}
             />
             <div className='form__footer'>
                 {
                     (type.isFetchingType && <div className='form__spinnerContainer'><SpinnerFacebook className='form__spinner'/></div>)
                     ||
-                    (formik.errors.name && <ErrorMessage message={formik.errors.name}/>)
+                    (formik.errors.name && <ErrorMessage message={i18n.t(formik.errors.name)}/>)
                     ||
-                    (error && <ErrorMessage message={error}/>)
+                    (error && <ErrorMessage message={i18n.t(error)}/>)
                 }
-                <Button className='form__btn' disabled={!formik.isValid || type.isFetchingType} type='submit' secondary>Добавить</Button>
+                <Button className='form__btn' disabled={!formik.isValid || type.isFetchingType} type='submit' secondary>{i18n.t('Add')}</Button>
             </div>   
         </form>
     )
