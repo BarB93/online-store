8a95d39f96f03ba2f1a46ff8fdd3bb4cdd0715e7
bbarovsky <barovskiyboris@gmail.com>
2022-06-29 17:31:53 +0300

    added checkRoleMiddleware for creating device

--- server/routes/deviceRouter.js
+++ server/routes/deviceRouter.js
@@ -1,9 +1,10 @@
 const Router           = require('express')
 const router           = new Router()
 const deviceController = require('../controllers/deviceController')
+const checkRoleMiddleware = require('../middleware/checkRoleMiddleware')
 
 router.get('/',    deviceController.getAll)
 router.get('/:id', deviceController.getOne)
-router.post('/',   deviceController.create)
+router.post('/', checkRoleMiddleware('ADMIN'), deviceController.create)
 
 module.exports = router
