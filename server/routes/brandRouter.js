1c7fdaadf67ae1e455bc813f0b87c9aa55505829
bbarovsky <barovskiyboris@gmail.com>
2022-06-29 17:31:22 +0300

    added checkRoleMiddleware for creating brand

--- server/routes/brandRouter.js
+++ server/routes/brandRouter.js
@@ -1,8 +1,9 @@
 const Router          = require('express')
 const router          = new Router()
 const brandController = require('../controllers/brandController')
+const checkRoleMiddleware = require('../middleware/checkRoleMiddleware')
 
 router.get('/',  brandController.getAll)
-router.post('/', brandController.create)
+router.post('/', checkRoleMiddleware('ADMIN'), brandController.create)
 
 module.exports = router 
