6a8099a8fb581c3742de8a62fef34fe0b15b231a
bbarovsky <barovskiyboris@gmail.com>
2022-07-02 01:55:10 +0300

    added in DeviceItem: fetch addDeviceToBasket in handlerBuy

--- client/src/components/Shop/DeviceItem/DeviceItem.js
+++ client/src/components/Shop/DeviceItem/DeviceItem.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {useNavigate} from 'react-router-dom'
 import { observer } from 'mobx-react-lite'
 import { useTranslation } from 'react-i18next'
 
-import { addDeviceToBasket } from '../../../http/basketAPI'
-import {DEVICE_ROUTE} from '../../../utils/consts'
+import { fetchOneDevice } from '../../../http/deviceAPI'
+import { Context } from '../../../index'
+import { createToast } from '../../UI/Toast/Toast'
+import { addDeviceToBasket, getQuantityBasketItems } from '../../../http/basketAPI'
+import { DEVICE_ROUTE } from '../../../utils/consts'
 import pricePrettify from '../../../utils/pricePrettify'
 import Button from '../../UI/Button/Button'
 
@@ -12,6 +15,7 @@ import star from '../../../assets/star.png'
 import styles from './DeviceItem.module.scss'
 
 const DeviceItem = observer(({device}) => {
+    const {basket, toast} = useContext(Context)
     const i18n = useTranslation()
     const navigate = useNavigate()
     const price = pricePrettify(device.price)
@@ -19,14 +23,13 @@ const DeviceItem = observer(({device}) => {
     const navigateToDevice = () => {navigate(DEVICE_ROUTE + '/' + device.id)}
     const imageURL = `${process.env.REACT_APP_API_URL}/${device.img}`
 
-    const handleBuy = () => {
+    const handlerBuy = () => {
         addDeviceToBasket(device)
-            .then(data => {
-                console.log('Успешно добавлен в корзину')
-            })
-            .catch(e => {
-                console.log('EErorrR',e)
-            })
+            .then(data => fetchOneDevice(data.deviceId))
+            .then(data => toast.addToast(createToast(i18n.t('Added to cart', {name: data.name}))))
+            .then(() => getQuantityBasketItems())
+            .then(data => basket.setQuantity(data))
+            .catch(e => console.error('Error in DeviceItem component:', e.message))
     }
 
     return (
@@ -35,7 +38,7 @@ const DeviceItem = observer(({device}) => {
                 <h3 className={styles.card__title} onClick={navigateToDevice}>{device.name}</h3>
                 <div className={styles.card__price}>{price}<small> ₽</small></div>
                 <div className={styles.card__buttons}>
-                    <Button className={styles.btnBuy} secondary onClick={handleBuy}>{i18n.t('Buy')}</Button>
+                    <Button className={styles.btnBuy} secondary onClick={handlerBuy}>{i18n.t('Buy')}</Button>
                     <Button className={styles.btnMore} onClick={navigateToDevice}>{i18n.t('More')}</Button>
                 </div> 
                 <img className={styles.card__image} onClick={navigateToDevice} src={imageURL} alt={device.name} />
