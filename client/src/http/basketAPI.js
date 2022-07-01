991174619890a2f4a57e334c819804d745e9dd9b
bbarovsky <barovskiyboris@gmail.com>
2022-07-02 01:42:07 +0300

    added asyns functions to basketAPI: addDeviceToBasket,getBasketItems, getQuantityBasketItems

--- client/src/http/basketAPI.js
+++ client/src/http/basketAPI.js
@@ -1,6 +1,16 @@
 import {$authHost} from './index'
 
 export const addDeviceToBasket = async (device) => {
-    const {data} = $authHost.post('/api/basket', device)
+    const {data} = await $authHost.post('/api/basket', device)
+    return data
+}
+
+export const getBasketItmes = async () => {
+    const {data} = await $authHost.get('/api/basket')
+    return data
+}
+
+export const getQuantityBasketItems = async () => {
+    const {data} = await $authHost.get('/api/basket/quantity')
     return data
 }
