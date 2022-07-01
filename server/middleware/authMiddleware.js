977f85be7f5cb3792e63a923b7c5e54f028e81c1
bbarovsky <barovskiyboris@gmail.com>
2022-07-02 01:38:24 +0300

    fixed in authMeddleware: used ApiError unauthorized

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken')
+const ApiError = require('../error/ApiError')
 
 module.exports = function(req, res, next) {
     if(req.method === 'OPTIONS') {
@@ -7,13 +8,13 @@ module.exports = function(req, res, next) {
     try {
         const token = req.headers.authorization.split(' ')[1]
         if(!token) {
-            return res.status(401).json({message: 'Not authorized'})
+            return next(ApiError.unauthorized())
         }
 
         const decoded = jwt.verify(token, process.env.SECRET_KEY)
         req.user = decoded
         next()
     } catch(e) {
-        res.status(401).json({message:'Not authorized'})
+        return next(ApiError.unauthorized())
     }
 }
