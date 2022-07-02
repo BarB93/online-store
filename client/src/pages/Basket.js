0b1fadb02c89bbb713ef1c91860b5847d5d793bd
bbarovsky <barovskiyboris@gmail.com>
2022-07-03 01:24:03 +0300

    added in Basket component: useEffect fetch basket items

--- client/src/pages/Basket.js
+++ client/src/pages/Basket.js
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+
+import { fetchBasketItems } from '../http/basketAPI'
+import { Context } from '../index'
 
 const Basket = () => {
+    const {basket, user} = useContext(Context)
+
+    useEffect(() => {
+        if(user.isAuth) {
+            fetchBasketItems()
+                .then(data => {
+                    basket.setDevices(data.devices)
+                    basket.setTotalQuantity(data.totalQuantity)
+                })
+        }
+    }, [user.isAuth])
+
     return (
         <div>BASKET PAGE</div>
     )
