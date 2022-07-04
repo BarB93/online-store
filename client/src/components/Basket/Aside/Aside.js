a592d928b02a678cd0ccccfcb8608dc49fc1a1c5
bbarovsky <barovskiyboris@gmail.com>
2022-07-04 18:36:28 +0300

    created Basket/Aside component

---
+++ client/src/components/Basket/Aside/Aside.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import styles from './Aside.module.scss'
+
+const Aside = () => {
+    return (
+        <div className={styles.aside}>Aside</div>
+    )
+}
+
+export default Aside
