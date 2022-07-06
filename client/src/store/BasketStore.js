5a6d11e072dac1e2571f945112a208d68b4d0160
bbarovsky <barovskiyboris@gmail.com>
2022-07-06 04:37:22 +0300

    added in BasketStore mobx: methods calcTotalQuantity,setDeviceQuantity and var isPending

--- client/src/store/BasketStore.js
+++ client/src/store/BasketStore.js
@@ -1,9 +1,11 @@
-import {makeAutoObservable} from 'mobx'
+import {makeAutoObservable, action} from 'mobx'
+import { setQuantity } from '../http/basketAPI'
 
 export default class UserStore {
     constructor() {
         this._devices = null
         this._totalQuantity = null
+        this._isPanding = false
         
         makeAutoObservable(this)
     }
@@ -20,5 +22,30 @@ export default class UserStore {
     }
     get devices() {
         return this._devices
+    }
+
+    calcTotalQuantity() {
+        const total = this.devices.reduce((acc, item) => acc + item.quantity, 0)
+        this.setTotalQuantity(total)
+    }
+
+    setDeviceQuantity(id, quantity) {
+        this._isPanding = true
+        setQuantity(id, quantity).then(
+            action('fetch success', data => {
+                if(data) {
+                    this.setDevices(this._devices.map(item => {
+                        if(item.id === id) {
+                            item.quantity = quantity
+                        }
+                        return item
+                    }))
+                }
+                this.calcTotalQuantity()
+            }),
+            action('fetch error', () => {}),
+        ).finally(() => {
+            this._isPanding = false
+        })
     }
 }
