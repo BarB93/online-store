f08bdaf8669ac0c56d637934d7cf4eff80ee8bce
bbarovsky <barovskiyboris@gmail.com>
2022-07-06 04:41:47 +0300

    added in basketRouter: put method for '/quantity'

--- server/routes/basketRouter.js
+++ server/routes/basketRouter.js
@@ -7,5 +7,6 @@ const checkRoleMiddleware = require('../middleware/checkRoleMiddleware')
 router.get('/', authMiddleware, basketController.getBasketDevices)
 router.post('/', checkRoleMiddleware(['ADMIN', 'USER']), basketController.addDevice)
 router.get('/quantity', authMiddleware, basketController.getQuantity)
+router.put('/quantity', authMiddleware, basketController.setQuantity)
 
 module.exports = router 
