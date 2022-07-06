4210dd00e6526addf906af6d6a69fd733c419ad6
bbarovsky <barovskiyboris@gmail.com>
2022-07-06 18:05:52 +0300

    added in BasketStore mobx: vars totalPrice, totalPriceWithoutDiscont, checkedDeviceIds

--- client/src/store/BasketStore.js
+++ client/src/store/BasketStore.js
@@ -1,32 +1,59 @@
 import {makeAutoObservable, action} from 'mobx'
 import { setQuantity } from '../http/basketAPI'
-
+import { DiSCOUNT } from '../utils/consts'
+import { getPriceWithoutDiscount } from '../utils/getPriceWithoutDiscount'
 export default class UserStore {
     constructor() {
         this._devices = null
+        this._checkedDeviceIds = null
         this._totalQuantity = null
         this._isPanding = false
+        this._totalPrice = 0
+        this._totalPriceWithoutDiscount = 0
         
         makeAutoObservable(this)
     }
 
+    // setters
     setTotalQuantity(num) {
         this._totalQuantity = num
     }
     setDevices(devices) {
         this._devices = devices
+        this._checkedDeviceIds = devices.map(item => item.id)
+        this.calcTotalPrice()
+    }
+    setTotalPrice(price) {
+        this._totalPrice = price
+    }
+    setTotalPriceWithoutDiscount(price) {
+        this._totalPriceWithoutDiscount = price
     }
    
+    // getters
     get totalQuantity() {
         return this._totalQuantity
     }
     get devices() {
         return this._devices
     }
+    get totalPrice() {
+        return this._totalPrice
+    }
+    get totalPriceWithoutDiscount() {
+        return this._totalPriceWithoutDiscount
+    }
 
+    // methods
     calcTotalQuantity() {
-        const total = this.devices.reduce((acc, item) => acc + item.quantity, 0)
+        const total = this._devices.reduce((acc, item) => acc + item.quantity, 0)
         this.setTotalQuantity(total)
+    }
+
+    calcTotalPrice() {
+        const total = this._devices.reduce((acc, item) => acc + item.price * item.quantity,0)
+        this.setTotalPrice(total)
+        this.setTotalPriceWithoutDiscount(getPriceWithoutDiscount(total, DiSCOUNT))
     }
 
     setDeviceQuantity(id, quantity) {
@@ -42,6 +69,7 @@ export default class UserStore {
                     }))
                 }
                 this.calcTotalQuantity()
+                this.calcTotalPrice()
             }),
             action('fetch error', () => {}),
         ).finally(() => {
