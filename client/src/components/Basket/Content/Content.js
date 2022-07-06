7f7d018540ecc0d9269012bc65a12cbf9c372252
bbarovsky <barovskiyboris@gmail.com>
2022-07-06 18:08:16 +0300

    added in Basket/Content component: base functional

--- client/src/components/Basket/Content/Content.js
+++ client/src/components/Basket/Content/Content.js
@@ -1,18 +1,29 @@
 import React, { useContext } from 'react'
+import { observer } from 'mobx-react-lite'
+import { useTranslation } from 'react-i18next'
 
 import { Context } from '../../../index'
 import BasketItem from '../BasketItem/BasketItem'
 
 import styles from './Content.module.scss'
 
-const Content = () => {
+const Content = observer(() => {
     const {basket} = useContext(Context)
+    const i18n = useTranslation()
 
     return (
         <div className={styles.content}>
-            {basket.devices && basket.devices.map(item => <BasketItem device={item} />)}
+            <div className={styles.content__header}>
+                <h3 className={styles.content__title}>{i18n.t('Basket')}</h3>
+                <label className='customCheckbox__label' htmlFor='all_goods'>
+                    <input className='customCheckbox__input' id='all_goods' type='checkbox' />
+                    <div className='customCheckbox__checkbox'></div>
+                    <span>{i18n.t('Select all')}</span>
+                </label>
+            </div>
+            {basket.devices && basket.devices.map(item => <BasketItem key={item.name} device={item} />)}
         </div>
     )
-}
+})
 
 export default Content
