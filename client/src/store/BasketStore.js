cc65aef614e1ffb5ce62eaa45e1dc3ca9af64c2d
bbarovsky <barovskiyboris@gmail.com>
2022-07-08 02:05:32 +0300

    added in BasketStore mobx: base functional

--- client/src/store/BasketStore.js
+++ client/src/store/BasketStore.js
@@ -4,12 +4,18 @@ import { DiSCOUNT } from '../utils/consts'
 import { getPriceWithoutDiscount } from '../utils/getPriceWithoutDiscount'
 export default class UserStore {
     constructor() {
-        this._devices = null
-        this._checkedDeviceIds = null
-        this._totalQuantity = null
-        this._isPanding = false
+        this._devices = []
+        this._order = []
+        this._isOrderInit = false
+        this._orderDeviceIds = []
+        this._isAllChecked = true
+        this._totalQuantity = 0
+        this._totalOrderQuantity = 0
+        this._isPandding = false
         this._totalPrice = 0
+        this._totalOrderPrice = 0
         this._totalPriceWithoutDiscount = 0
+        this._totalOrderPriceWithoutDiscount = 0
         
         makeAutoObservable(this)
     }
@@ -18,21 +24,63 @@ export default class UserStore {
     setTotalQuantity(num) {
         this._totalQuantity = num
     }
+
+    setTotalOrderQuantity(num) {
+        this._totalOrderQuantity = num
+    }
+
     setDevices(devices) {
         this._devices = devices
-        this._checkedDeviceIds = devices.map(item => item.id)
         this.calcTotalPrice()
+    
+        if(!this._isOrderInit) {
+            this.setOrderDeviceIds(devices.map(item => item.id))
+            this._isOrderInit = true
+        }else {
+            this.updateOrder()
+        }
     }
+
     setTotalPrice(price) {
         this._totalPrice = price
     }
+
+    setTotalOrderPrice(price) {
+        this._totalOrderPrice = price
+    }
+
     setTotalPriceWithoutDiscount(price) {
         this._totalPriceWithoutDiscount = price
     }
-   
+
+    setTotalOrderPriceWithoutDiscount(price) {
+        this._totalOrderPriceWithoutDiscount = price
+    }
+
+    setOrderDeviceIds(array) {
+        this._orderDeviceIds = array
+        this.setIsAllChecked(this._devices.every(item => array.includes(item.id)))
+        this.setOrder(this._devices.filter(item => this._orderDeviceIds.includes(item.id)))
+    }
+    setOrder(devices) {
+        this._order = devices
+        this.calcTotalOrderPrice()
+        this.calcTotalOrderQuantity()
+    }
+
+    setIsOrderInit(bool) {
+        this._isOrderInit = bool
+    }
+    setIsAllChecked(bool) {
+        this._isAllChecked = bool
+    }
+
     // getters
     get totalQuantity() {
         return this._totalQuantity
+    }
+    get totalOrderQuantity() {
+        return this._totalOrderQuantity
     }
     get devices() {
         return this._devices
@@ -40,24 +88,63 @@ export default class UserStore {
     get totalPrice() {
         return this._totalPrice
     }
+    get totalOrderPrice() {
+        return this._totalOrderPrice
+    }
     get totalPriceWithoutDiscount() {
         return this._totalPriceWithoutDiscount
     }
+    get totalOrderPriceWithoutDiscount() {
+        return this._totalOrderPriceWithoutDiscount
+    }
+    get orderDeviceIds() {
+        return this._orderDeviceIds
+    }
+    get isAllChecked() {
+        return this._isAllChecked
+    }
+    get isOrderInit() {
+        return this._isOrderInit
+    }
 
     // methods
+    addToOrder(id) {
+        if(!this._orderDeviceIds.includes(id)){
+            this.setOrderDeviceIds([...this._orderDeviceIds, id])
+        }
+    }
+
+    removeFromOrder(id) {
+        this.setOrderDeviceIds(this._orderDeviceIds.filter(orderId => orderId !== id))
+    }
+
     calcTotalQuantity() {
         const total = this._devices.reduce((acc, item) => acc + item.quantity, 0)
         this.setTotalQuantity(total)
     }
 
+    calcTotalOrderQuantity() {
+        const total = this._order.reduce((acc, item) => acc + item.quantity, 0)
+        this.setTotalOrderQuantity(total)
+    }
     calcTotalPrice() {
         const total = this._devices.reduce((acc, item) => acc + item.price * item.quantity,0)
         this.setTotalPrice(total)
         this.setTotalPriceWithoutDiscount(getPriceWithoutDiscount(total, DiSCOUNT))
     }
 
+    calcTotalOrderPrice() {
+        const total = this._order.reduce((acc, item) => acc + item.price * item.quantity,0)
+        this.setTotalOrderPrice(total)
+        this.setTotalOrderPriceWithoutDiscount(getPriceWithoutDiscount(total, DiSCOUNT))
+    }
+
+    updateOrder() {
+        this.setOrder(this._devices.filter(item => this._orderDeviceIds.includes(item.id)))
+    }
+
     setDeviceQuantity(id, quantity) {
-        this._isPanding = true
+        this._isPandding = true
         setQuantity(id, quantity).then(
             action('fetch success', data => {
                 if(data) {
@@ -73,7 +160,15 @@ export default class UserStore {
             }),
             action('fetch error', () => {}),
         ).finally(() => {
-            this._isPanding = false
+            this._isPandding = false
         })
+    }
+    
+    clearAllOrder() {
+        this.setOrderDeviceIds([])
+    }
+
+    checkAllOrder() {
+        this.setOrderDeviceIds(this._devices.map(item => item.id))
     }
 }
