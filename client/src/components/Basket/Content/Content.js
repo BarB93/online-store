5c78610963d07343c8abdf0c0162582e11af23ed
bbarovsky <barovskiyboris@gmail.com>
2022-07-08 02:10:27 +0300

    added in Basket/Content component: change handler for checkbox

--- client/src/components/Basket/Content/Content.js
+++ client/src/components/Basket/Content/Content.js
@@ -10,13 +10,20 @@ import styles from './Content.module.scss'
 const Content = observer(() => {
     const {basket} = useContext(Context)
     const i18n = useTranslation()
+    const selectAllGoodsChangeHandler = (e) => {
+        if(e.target.checked) {
+            basket.checkAllOrder()
+        } else {
+            basket.clearAllOrder()
+        }
+    }
 
     return (
         <div className={styles.content}>
             <div className={styles.content__header}>
                 <h3 className={styles.content__title}>{i18n.t('Basket')}</h3>
                 <label className='customCheckbox__label' htmlFor='all_goods'>
-                    <input className='customCheckbox__input' id='all_goods' type='checkbox' />
+                    <input className='customCheckbox__input' id='all_goods' type='checkbox' checked={basket.isAllChecked} onChange={selectAllGoodsChangeHandler}/>
                     <div className='customCheckbox__checkbox'></div>
                     <span>{i18n.t('Select all')}</span>
                 </label>
