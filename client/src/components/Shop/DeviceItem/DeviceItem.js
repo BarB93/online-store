358ca62faa224a2e3e89b798236c536f2cc76d52
bbarovsky <barovskiyboris@gmail.com>
2022-07-10 21:26:55 +0300

    DeviceItem component: refactor, use NavLink

--- client/src/components/Shop/DeviceItem/DeviceItem.js
+++ client/src/components/Shop/DeviceItem/DeviceItem.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import {useNavigate} from 'react-router-dom'
+import {NavLink, useNavigate} from 'react-router-dom'
 import { observer } from 'mobx-react-lite'
 import { useTranslation } from 'react-i18next'
 
@@ -14,10 +14,8 @@ import styles from './DeviceItem.module.scss'
 const DeviceItem = observer(({device}) => {
     const {basket} = useContext(Context)
     const i18n = useTranslation()
-    const navigate = useNavigate()
     const price = pricePrettify(device.price)
 
-    const navigateToDevice = () => {navigate(DEVICE_ROUTE + '/' + device.id)}
     const imageURL = `${process.env.REACT_APP_API_URL}/${device.img}`
 
     const handlerBuy = () => {
@@ -26,13 +24,13 @@ const DeviceItem = observer(({device}) => {
 
     return (
         <article className={styles.card}>
-                <h3 className={styles.card__title} onClick={navigateToDevice}>{device.name}</h3>
+                <h3 className={styles.card__title}><NavLink to={`${DEVICE_ROUTE}/${device.id}`}>{device.name}</NavLink></h3>
                 <div className={styles.card__price}>{price}<small> ₽</small></div>
                 <div className={styles.card__buttons}>
-                    <Button className={styles.btnBuy} secondary onClick={handlerBuy}>{i18n.t('Buy')}</Button>
-                    <Button className={styles.btnMore} onClick={navigateToDevice}>{i18n.t('More')}</Button>
+                    <Button className={styles.card__btnBuy} secondary onClick={handlerBuy}>{i18n.t('Buy')}</Button>
+                    <NavLink className={styles.card__moreLink} to={`${DEVICE_ROUTE}/${device.id}`}><Button className={styles.card__btnMore}>{i18n.t('More')}</Button></NavLink>   
                 </div> 
-                <img className={styles.card__image} onClick={navigateToDevice} src={imageURL} alt={device.name} />
+                <NavLink to={`${DEVICE_ROUTE}/${device.id}`}><img className={styles.card__image} src={imageURL} alt={device.name} /></NavLink>
                 <div className={styles.card__rating}>
                     <img src={star} alt={i18n.t('Rating')} />
                     {device.rating && <span>{device.rating.toFixed(1)}</span>}
