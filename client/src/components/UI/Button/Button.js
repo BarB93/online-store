db04ce31ff2e8d90f99ccdcad376e8572b143081
bbarovsky <barovskiyboris@gmail.com>
2022-07-15 02:07:24 +0300

    added styles for Button component

--- client/src/components/UI/Button/Button.js
+++ client/src/components/UI/Button/Button.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 import styles from './Button.module.scss'
 
-const Button = ({onClick, children, className, secondary, ...props}) => {
+const Button = ({onClick, children, className, secondary, bold, ...props}) => {
     let classes = `${styles.btn} `
     classes += secondary ? `${styles.secondary} ` : ''
+    classes += bold ? `${styles.bold} ` : ''
     classes += className ? className : ''
     
     return (
